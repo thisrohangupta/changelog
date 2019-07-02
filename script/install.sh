419d5862f8f153cff4ce535a1c325e5195e767b9
Rohan Gupta <rohan.gupta@harness.io>
2019-07-02 13:54:59 -0700

    minor changes to the install script

--- script/install.sh
+++ script/install.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-cd
 
 # Install Homebrew
 
@@ -16,25 +15,25 @@ sysctl -a | grep machdep.cpu.features
 
 # Install Kubectl
 
+echo "Installing Kubectl, Kubernetes command line tool"
 curl -LO https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl
 
 
 # Install Minikube 
-
+echo "Installing Minikube for local kubernetes development"
 brew cask install minikube 
 
-
 # Install Kubectx && Kubens
-
+echo "Installing power user command line tool for Kubernetes Kubectx"
 brew install kubectx
 
 # Install Helm
-
+echo "Installing helm, Kubernetes package manager"
 brew install kubernetes-helm
 
 
 # Install AWS CLI
-
+echo "Installing the AWS CLI tool"
 curl "https://s3.amazonaws.com/aws-cli/awscli-bundle.zip" -o "awscli-bundle.zip"
 
 unzip awscli-bundle.zip
@@ -42,7 +41,9 @@ unzip awscli-bundle.zip
 sudo ./awscli-bundle/install -i /usr/local/aws -b /usr/local/bin/aws
 
 # Install Google Cloud Command line
-
+echo "Installing the Google Command line tool"
 curl google-cloud-sdk-253.0.0-darwin-x86_64.tar.gz | tar xvz
 ./google-cloud-sdk/install.sh
 
+
+echo "***** Blackbelt Install Script Complete *****"
